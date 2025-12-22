ef8fb8e78501bb924dcb544a063c1e5ce8247bb7
lorenzo689 <lorenzo.negri@web.de>
2025-12-22 17:10:28 +0100

    Fixing typing errors

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -28,7 +28,7 @@ const Sidebar = ({expand, setExpand}: SidebarProps) => {
 
         <button className={`mt-8 flex items-center justify-center cursor-pointer ${expand ? "bg-primary hover:opacity-90 rounded-2xl gap-2 p-2.5 w-max" : "group relative h-9 w-9 mx-auto hover:bg-gray-500/30 rounded-lg"}`}>
           <Image className={expand ? 'w-6' : 'w-7'} src={expand ? assets.chat_icon : assets.chat_icon_dull} alt=''/>
-          <div className='absolute w-max -top-12 -right-12 opacity-0 group-hover:opaciy-100 transition bg-black text-white text-sm px-3 py-2 rounded-lg shadow-lg pointer-events-none'>
+          <div className='absolute w-max -top-12 -right-12 opacity-0 group-hover:opacity-100 transition bg-black text-white text-sm px-3 py-2 rounded-lg shadow-lg pointer-events-none'>
             New Chat
             <div className='w-3 h-3 absolute bg-black rotate-45 left-4 -bottom-1.5'></div>  
           </div>
@@ -43,12 +43,12 @@ const Sidebar = ({expand, setExpand}: SidebarProps) => {
 
     <div>
       <div className={`flex items-center cursor-pointer group relative ${expand ? "gap-1 text-white/80 text-sm p-2.5 border border-primary rounded-lg" : "h-10 w-10 mx-auto hover:bg-gray-500/30 rounded-lg"}`}>
-          <Image className={expand ? "w-5" : "w-6.5 mx-auto"} src={expand ? assets.phone_icon : assets.phone_icon_dull} alt=''/>
+          <Image className={expand ? "w-5" : "w-7 mx-auto"} src={expand ? assets.phone_icon : assets.phone_icon_dull} alt=''/>
         <div className={`absolute -top-60 pb-8 ${!expand && "-right-40"} opacity-0 group-hover:opacity-100 hidden group-hover:block transition`}>
           <div className='relative w-max bg-black text-white text-sm p-3 rounded-lg shadow-lg'>
             <Image src={assets.qrcode} alt='' className='w-44'/>
             <p>Scan to get Deepseek App</p>
-            <div className={`w-3 h-3 abolute bg-black rotate-45 ${expand ? "right-1/2" : "left-4"} -bottom-1.5`}></div>
+            <div className={`w-3 h-3 absolute bg-black rotate-45 ${expand ? "right-1/2" : "left-4"} -bottom-1.5`}></div>
           </div>
         </div>
         {expand && <> <span>Get App</span><Image alt='' src={assets.new_icon}/> </>}
